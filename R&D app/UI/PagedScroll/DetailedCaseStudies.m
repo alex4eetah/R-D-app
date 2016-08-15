2773b20c250e289ecdd14c27576e2992e36c157e
AlexCheetah <AlexCheetah@alexcheetahs-mac.local>
2016-08-15 10:41:36 +0300

    Idea of creating views in scroll

--- R&D app/UI/PagedScroll/DetailedCaseStudies.m
+++ R&D app/UI/PagedScroll/DetailedCaseStudies.m
@@ -7,7 +7,7 @@
 //
 
 #import "DetailedCaseStudies.h"
-#import "DetailedCaseStudyViewController.h"
+#import "DetailedCaseStudyView.h"
 
 @interface DetailedCaseStudies () <UIScrollViewDelegate>
 
@@ -42,9 +42,9 @@
                    @"img":[UIImage imageNamed:@"2016072854009.png"],
                    @"desc":@"VoiceMyBot"}, nil];
     
-    NSString * storyboardName = @"Main";
+   /* NSString * storyboardName = @"Main";
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:storyboardName bundle: nil];
-    
+
     NSMutableArray *arr = [[NSMutableArray alloc] init];
     
     for (int i=0; i<5; i++) {
@@ -54,12 +54,102 @@
         vc.view.frame = CGRectMake(self.scroll.frame.size.width * i, 0, self.scroll.frame.size.width, self.scroll.frame.size.height);
         
         
-        [self.scroll addSubview:vc.view];
+        
+        
+        [self.scroll addSubview:];
         [arr addObject:vc];
     }
 
-    self.scroll.contentSize = CGSizeMake(self.scroll.frame.size.width * pics.count, self.scroll.frame.size.height);
+    self.scroll.contentSize = CGSizeMake(self.scroll.frame.size.width * pics.count, self.scroll.frame.size.height);*/
     
+    NSMutableArray *viewsArr = [[NSMutableArray alloc] init];
+    
+    DetailedCaseStudyView *viewToAdd = [[DetailedCaseStudyView alloc] init];
+    viewToAdd.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.scroll addSubview:viewToAdd];
+    [viewsArr addObject:viewToAdd];
+    
+    DetailedCaseStudyView *viewToAdd1 = [[DetailedCaseStudyView alloc] init];
+    viewToAdd1.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.scroll addSubview:viewToAdd1];
+    [viewsArr addObject:viewToAdd1];
+    
+    DetailedCaseStudyView *viewToAdd2 = [[DetailedCaseStudyView alloc] init];
+    viewToAdd2.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.scroll addSubview:viewToAdd2];
+    [viewsArr addObject:viewToAdd2];
+    
+    [self.scroll addConstraint:[NSLayoutConstraint constraintWithItem:viewsArr.firstObject
+                                                            attribute:NSLayoutAttributeWidth
+                                                            relatedBy:NSLayoutRelationEqual
+                                                               toItem:self.scroll
+                                                            attribute:NSLayoutAttributeWidth
+                                                           multiplier:1.0
+                                                             constant:0.0]];
+    
+    [self.scroll addConstraint:[NSLayoutConstraint constraintWithItem:viewsArr.firstObject
+                                                            attribute:NSLayoutAttributeHeight
+                                                            relatedBy:NSLayoutRelationEqual
+                                                               toItem:self.scroll
+                                                            attribute:NSLayoutAttributeWidth
+                                                           multiplier:1.0
+                                                             constant:0.0]];
+    
+    [self.scroll addConstraint:[NSLayoutConstraint constraintWithItem:viewsArr.firstObject
+                                                            attribute:NSLayoutAttributeLeading
+                                                            relatedBy:NSLayoutRelationEqual
+                                                               toItem:self.scroll
+                                                            attribute:NSLayoutAttributeLeading
+                                                           multiplier:1.0
+                                                             constant:53]];
+    
+    [self.scroll addConstraint:[NSLayoutConstraint constraintWithItem:viewsArr.firstObject
+                                                            attribute:NSLayoutAttributeTop
+                                                            relatedBy:NSLayoutRelationEqual
+                                                               toItem:self.scroll
+                                                            attribute:NSLayoutAttributeTop
+                                                           multiplier:1.0
+                                                             constant:32]];
+    
+    for (int i = 1; i < viewsArr.count; i ++) {
+        [self.scroll addConstraint:[NSLayoutConstraint constraintWithItem:viewsArr[i]
+                                                                attribute:NSLayoutAttributeWidth
+                                                                relatedBy:NSLayoutRelationEqual
+                                                                   toItem:self.scroll
+                                                                attribute:NSLayoutAttributeWidth
+                                                               multiplier:1.0
+                                                                 constant:0.0]];
+        
+        [self.scroll addConstraint:[NSLayoutConstraint constraintWithItem:viewsArr[i]
+                                                                attribute:NSLayoutAttributeHeight
+                                                                relatedBy:NSLayoutRelationEqual
+                                                                   toItem:self.scroll
+                                                                attribute:NSLayoutAttributeWidth
+                                                               multiplier:1.0
+                                                                 constant:0.0]];
+        
+        [self.scroll addConstraint:[NSLayoutConstraint constraintWithItem:viewsArr[i]
+                                                                attribute:NSLayoutAttributeLeading
+                                                                relatedBy:NSLayoutRelationEqual
+                                                                   toItem:viewsArr[i-1]
+                                                                attribute:NSLayoutAttributeLeading
+                                                               multiplier:1.0
+                                                                 constant:53]];
+        
+        [self.scroll addConstraint:[NSLayoutConstraint constraintWithItem:viewsArr[i]
+                                                                attribute:NSLayoutAttributeTop
+                                                                relatedBy:NSLayoutRelationEqual
+                                                                   toItem:self.scroll
+                                                                attribute:NSLayoutAttributeTop
+                                                               multiplier:1.0
+                                                                 constant:32]];
+    }
+    
+    
+    
+    
+    
+    self.scroll.contentSize = CGSizeMake(self.scroll.frame.size.width * 3, self.scroll.frame.size.height);
 }
 
 
